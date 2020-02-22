aaa5eef833b2fd5d73169e6c02155b66578d7cac
Garrett Filson <gf17@my.fsu.edu>
2020-02-21 19:54:30 -0500

    Add files via upload

---
+++ Statcast18and19Scrape.R
@@ -0,0 +1,124 @@
+library(baseballr)
+
+date328403 = baseballr::scrape_statcast_savant(start_date = '2019-03-28',
+                                               end_date = '2019-04-03', player_type = 'hitter')
+date404410 = baseballr::scrape_statcast_savant(start_date = '2019-04-04',
+                                               end_date = '2019-04-10', player_type = 'hitter')
+date411417 = baseballr::scrape_statcast_savant(start_date = '2019-04-11',
+                                               end_date = '2019-04-17', player_type = 'hitter')
+date418424 = baseballr::scrape_statcast_savant(start_date = '2019-04-18',
+                                               end_date = '2019-04-24', player_type = 'hitter')
+date425501 = baseballr::scrape_statcast_savant(start_date = '2019-04-25',
+                                               end_date = '2019-05-01', player_type = 'hitter')
+date502508 = baseballr::scrape_statcast_savant(start_date = '2019-05-02',
+                                               end_date = '2019-05-08', player_type = 'hitter')
+date509515 = baseballr::scrape_statcast_savant(start_date = '2019-05-09',
+                                               end_date = '2019-05-15', player_type = 'hitter')
+date516522 = baseballr::scrape_statcast_savant(start_date = '2019-05-16',
+                                               end_date = '2019-05-22', player_type = 'hitter')
+date523529 = baseballr::scrape_statcast_savant(start_date = '2019-05-23',
+                                               end_date = '2019-05-29', player_type = 'hitter')
+date530605 = baseballr::scrape_statcast_savant(start_date = '2019-05-30',
+                                               end_date = '2019-06-05', player_type = 'hitter')
+date606612 = baseballr::scrape_statcast_savant(start_date = '2019-06-06',
+                                               end_date = '2019-06-12', player_type = 'hitter')
+date613619 = baseballr::scrape_statcast_savant(start_date = '2019-06-13',
+                                               end_date = '2019-06-19', player_type = 'hitter')
+date620626 = baseballr::scrape_statcast_savant(start_date = '2019-06-20',
+                                               end_date = '2019-06-26', player_type = 'hitter')
+date627703 = baseballr::scrape_statcast_savant(start_date = '2019-06-27',
+                                               end_date = '2019-07-03', player_type = 'hitter')
+date704710 = baseballr::scrape_statcast_savant(start_date = '2019-07-04',
+                                               end_date = '2019-07-10', player_type = 'hitter')
+date711717 = baseballr::scrape_statcast_savant(start_date = '2019-07-11',
+                                               end_date = '2019-07-17', player_type = 'hitter')
+date718724 = baseballr::scrape_statcast_savant(start_date = '2019-07-18',
+                                               end_date = '2019-07-24', player_type = 'hitter')
+date725731 = baseballr::scrape_statcast_savant(start_date = '2019-07-25',
+                                               end_date = '2019-07-31', player_type = 'hitter')
+date801807 = baseballr::scrape_statcast_savant(start_date = '2019-08-01',
+                                               end_date = '2019-08-07', player_type = 'hitter')
+date808814 = baseballr::scrape_statcast_savant(start_date = '2019-08-08',
+                                               end_date = '2019-08-14', player_type = 'hitter')
+date815821 = baseballr::scrape_statcast_savant(start_date = '2019-08-15',
+                                               end_date = '2019-08-21', player_type = 'hitter')
+date822828 = baseballr::scrape_statcast_savant(start_date = '2019-08-22',
+                                               end_date = '2019-08-28', player_type = 'hitter')
+date829904 = baseballr::scrape_statcast_savant(start_date = '2019-08-29',
+                                               end_date = '2019-09-04', player_type = 'hitter')
+date905911 = baseballr::scrape_statcast_savant(start_date = '2019-09-05',
+                                               end_date = '2019-09-11', player_type = 'hitter')
+date912918 = baseballr::scrape_statcast_savant(start_date = '2019-09-12',
+                                               end_date = '2019-09-18', player_type = 'hitter')
+date919925 = baseballr::scrape_statcast_savant(start_date = '2019-09-19',
+                                               end_date = '2019-09-25', player_type = 'hitter')
+date926929 = baseballr::scrape_statcast_savant(start_date = '2019-09-26',
+                                               end_date = '2019-09-29', player_type = 'hitter')
+date329404 = baseballr::scrape_statcast_savant(start_date = '2018-03-29',
+                                               end_date = '2018-04-04', player_type = 'hitter')
+date405411 = baseballr::scrape_statcast_savant(start_date = '2018-04-05',
+                                               end_date = '2018-04-11', player_type = 'hitter')
+date412418 = baseballr::scrape_statcast_savant(start_date = '2018-04-12',
+                                               end_date = '2018-04-18', player_type = 'hitter')
+date419425 = baseballr::scrape_statcast_savant(start_date = '2018-04-19',
+                                               end_date = '2018-04-25', player_type = 'hitter')
+date426502 = baseballr::scrape_statcast_savant(start_date = '2018-04-26',
+                                               end_date = '2018-05-02', player_type = 'hitter')
+date503509 = baseballr::scrape_statcast_savant(start_date = '2018-05-03',
+                                               end_date = '2018-05-09', player_type = 'hitter')
+date510516 = baseballr::scrape_statcast_savant(start_date = '2018-05-10',
+                                               end_date = '2018-05-16', player_type = 'hitter')
+date517523 = baseballr::scrape_statcast_savant(start_date = '2018-05-17',
+                                               end_date = '2018-05-23', player_type = 'hitter')
+date524530 = baseballr::scrape_statcast_savant(start_date = '2018-05-24',
+                                               end_date = '2018-05-30', player_type = 'hitter')
+date531606 = baseballr::scrape_statcast_savant(start_date = '2018-05-31',
+                                               end_date = '2018-06-06', player_type = 'hitter')
+date607613 = baseballr::scrape_statcast_savant(start_date = '2018-06-07',
+                                               end_date = '2018-06-13', player_type = 'hitter')
+date614620 = baseballr::scrape_statcast_savant(start_date = '2018-06-14',
+                                               end_date = '2018-06-20', player_type = 'hitter')
+date621627 = baseballr::scrape_statcast_savant(start_date = '2018-06-21',
+                                               end_date = '2018-06-27', player_type = 'hitter')
+date628704 = baseballr::scrape_statcast_savant(start_date = '2018-06-28',
+                                               end_date = '2018-07-04', player_type = 'hitter')
+date705711 = baseballr::scrape_statcast_savant(start_date = '2018-07-05',
+                                               end_date = '2018-07-11', player_type = 'hitter')
+date712718 = baseballr::scrape_statcast_savant(start_date = '2018-07-12',
+                                               end_date = '2018-07-18', player_type = 'hitter')
+date719725 = baseballr::scrape_statcast_savant(start_date = '2018-07-19',
+                                               end_date = '2018-07-25', player_type = 'hitter')
+date726801 = baseballr::scrape_statcast_savant(start_date = '2018-07-26',
+                                               end_date = '2018-08-01', player_type = 'hitter')
+date802808 = baseballr::scrape_statcast_savant(start_date = '2018-08-02',
+                                               end_date = '2018-08-08', player_type = 'hitter')
+date809815 = baseballr::scrape_statcast_savant(start_date = '2018-08-09',
+                                               end_date = '2018-08-15', player_type = 'hitter')
+date816822 = baseballr::scrape_statcast_savant(start_date = '2018-08-16',
+                                               end_date = '2018-08-22', player_type = 'hitter')
+date823829 = baseballr::scrape_statcast_savant(start_date = '2018-08-23',
+                                               end_date = '2018-08-29', player_type = 'hitter')
+date830905 = baseballr::scrape_statcast_savant(start_date = '2018-08-30',
+                                               end_date = '2018-09-05', player_type = 'hitter')
+date906912 = baseballr::scrape_statcast_savant(start_date = '2018-09-06',
+                                               end_date = '2018-09-12', player_type = 'hitter')
+date913919 = baseballr::scrape_statcast_savant(start_date = '2018-09-13',
+                                               end_date = '2018-09-19', player_type = 'hitter')
+date920926 = baseballr::scrape_statcast_savant(start_date = '2018-09-20',
+                                               end_date = '2018-09-26', player_type = 'hitter')
+date9271001 = baseballr::scrape_statcast_savant(start_date = '2018-09-27',
+                                               end_date = '2018-10-01', player_type = 'hitter')
+
+SavantData18and19 = rbind(date328403, date404410, date411417, date418424, 
+                           date425501, date502508, date509515, date516522, 
+                           date523529, date530605, date606612, date613619, 
+                           date620626, date627703, date704710, date711717, 
+                           date718724, date725731, date801807, date808814, 
+                           date815821, date822828, date829904, date905911, 
+                           date912918, date919925, date926929, date329404, date405411, date412418, date419425, 
+                           date426502, date503509, date510516, date517523, 
+                           date524530, date531606, date607613, date614620, 
+                           date621627, date628704, date705711, date712718, 
+                           date719725, date726801, date802808, date809815, 
+                           date816822, date823829, date830905, date906912, 
+                           date913919, date920926, date9271001)
