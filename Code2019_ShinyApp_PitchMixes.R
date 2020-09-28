77171c453ee58458ef3d406d8c8ce2dd885583a9
Garrett Filson <gf17@my.fsu.edu>
2020-09-28 19:25:08 -0400

    Add files via upload

--- Code2019_ShinyApp_PitchMixes.R
+++ Code2019_ShinyApp_PitchMixes.R
@@ -1,3 +1,15 @@
+Savant_PitchMix_2019 <- Savant_PitchMix %>%
+    mutate(player_name = paste(first_name, last_name, sep = " ")) %>%
+    arrange(player_name) %>%
+    rename(PercentThrown = n_ff_formatted,
+           FF_Velo = ff_avg_speed,
+           FF_Spin_Rate = ff_avg_spin,
+           FF_Horiz_Break = ff_avg_break_x, 
+           FF_Vert_Break = ff_avg_break_z,
+           First_Name = first_name,
+           Last_Name = last_name,
+           Year = year)
+
 
 library(shiny)
 
